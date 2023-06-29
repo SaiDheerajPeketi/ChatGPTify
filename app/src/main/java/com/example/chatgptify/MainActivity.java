9515e879b6817dbc7f7aa510dcc7ff86ea537305
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-06-29 20:59:04 +0530

    Initialization

--- app/src/main/java/com/example/chatgptify/MainActivity.java
+++ app/src/main/java/com/example/chatgptify/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.chatgptify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView chatRecyclerView;
+    private TextView welcomeTextView;
+    private EditText messageEditText;
+    private ImageButton sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerView);
+        welcomeTextView = findViewById(R.id.welcomeTextView);
+        messageEditText = findViewById(R.id.messageEditText);
+        sendButton = findViewById(R.id.sendImageButton);
+
     }
 }
