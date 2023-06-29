4ad221dfc22c45c0768037c9846307fb9de890ec
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-06-29 22:09:24 +0530

    Integration of API Complete

--- app/src/main/java/com/example/chatgptify/MainActivity.java
+++ app/src/main/java/com/example/chatgptify/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatgptify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +13,20 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView chatRecyclerView;
@@ -21,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton sendButton;
     private ArrayList<Message> messageArrayList;
     private MessageAdapter messageAdapter;
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         //Data Holder Initialization
         messageArrayList = new ArrayList<Message>();
 
+
         //Recycler View Setup
         messageAdapter = new MessageAdapter(messageArrayList);
         chatRecyclerView.setAdapter(messageAdapter);
@@ -47,9 +64,10 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String question = messageEditText.getText().toString();
+                String question = messageEditText.getText().toString().trim();
                 addToChat(question, Message.SENT_BY_ME);
                 messageEditText.setText("");
+                callAPI(question);
                 welcomeTextView.setVisibility(View.GONE);
             }
         });
@@ -64,6 +82,59 @@ public class MainActivity extends AppCompatActivity {
                 chatRecyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
             }
         });
-
     }
+
+    private void addResponse(String message) {
+        messageArrayList.remove(messageArrayList.size()-1);
+        addToChat(message,Message.SENT_BY_BOT);
+    }
+
+    private void callAPI(String question){
+        messageArrayList.add(new Message("Typing...",Message.SENT_BY_BOT));
+        messageAdapter.notifyDataSetChanged();
+
+        JSONObject jsonBody = new JSONObject();
+        try{
+            jsonBody.put("model","text-davinci-003");
+            jsonBody.put("prompt",question);
+            jsonBody.put("max_tokens",4000);
+            jsonBody.put("temperature",0);
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to Load Response due to " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    addResponse("Failed to Load Response due to " + response.body().toString());
+                }
+            }
+        });
+
+        }
+
 }
